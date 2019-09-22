3acf9ec0f3fc15f15dd54af1b3ec9272cdc47027
root <root@sulai.myvserver.online>
2019-09-22 18:31:25 +0200

    first running page

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace WebDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+		var host = new WebHostBuilder()
+		  .UseKestrel()
+		  .UseUrls("http://*:80")
+		  .UseContentRoot(Directory.GetCurrentDirectory())
+		  .UseStartup<Startup>()
+		  .Build();
+ 
+	    	host.Run();
+        }
+
+    }
+}
